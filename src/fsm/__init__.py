65e1abdbcf90c0743f97003da13efd852c2c7a64
alvarezdqal <alvarezdqal@gmail.com>
2021-06-04 17:17:53 -0400

    imported machines to __init__

--- src/fsm/__init__.py
+++ src/fsm/__init__.py
@@ -1,1 +1,3 @@
 from fsm.finite_state_acceptor import FiniteStateAcceptor  # noqa: F401
+from fsm.finite_state_machine import FiniteStateMachine  # noqa: F401
+from fsm.finite_state_transducer import FiniteStateTransducer  # noqa: F401
