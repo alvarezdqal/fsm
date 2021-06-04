620404d2621e6bb00f7175a9c33822f7a5004050
alvarezdqal <alvarezdqal@gmail.com>
2021-06-03 22:23:23 -0400

    test boilerplate

---
+++ test/test_finite_state_machine.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from fsm.finite_state_machine import (
+    FiniteStateMachine,
+    validate_alphabet,
+    validate_final_states,
+    validate_initial_state,
+    validate_state_transition_function,
+    validate_states,
+)
+
+
+class TestFiniteStateMachine(TestCase):
+    pass
